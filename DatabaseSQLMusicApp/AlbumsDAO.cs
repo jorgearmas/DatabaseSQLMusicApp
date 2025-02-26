2ff31b8f6b46c1637ccd878d1a546d91c543cc91
jorgearmas <jorgearmasc0496@gmail.com>
2025-02-26 09:47:43 -0600

    Initial commit

---
+++ DatabaseSQLMusicApp/AlbumsDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSQLMusicApp
+{
+    internal class AlbumsDAO
+    {
+        // v1 contains fake data
+        List<Album> albums = new List<Album>
+        {
+
+        };
+    }
+}
