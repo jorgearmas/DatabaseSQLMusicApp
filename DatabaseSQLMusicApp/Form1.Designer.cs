1681f54e0e70f0d93deb526d1e26a0d33fa5c061
jorgearmas <jorgearmasc0496@gmail.com>
2025-02-26 16:44:39 -0600

    txtbox added, buttons added

--- DatabaseSQLMusicApp/Form1.Designer.cs
+++ DatabaseSQLMusicApp/Form1.Designer.cs
@@ -30,7 +30,11 @@
         {
             button1 = new Button();
             dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -46,28 +50,63 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(353, 16);
+            dataGridView1.Location = new Point(207, 62);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(553, 188);
+            dataGridView1.Size = new Size(866, 201);
             dataGridView1.TabIndex = 1;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(495, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(732, 18);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 62);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(180, 201);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(918, 450);
+            ClientSize = new Size(1086, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
             Controls.Add(dataGridView1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Button button2;
+        private PictureBox pictureBox1;
     }
 }
