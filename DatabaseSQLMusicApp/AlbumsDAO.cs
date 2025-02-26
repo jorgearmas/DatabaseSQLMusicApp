a4a71252c19a058df9cc46d68147ab96341c26b3
jorgearmas <jorgearmasc0496@gmail.com>
2025-02-26 16:34:43 -0600

    getAllAlbums and searchTitle methonds implemented

--- DatabaseSQLMusicApp/AlbumsDAO.cs
+++ DatabaseSQLMusicApp/AlbumsDAO.cs
@@ -3,15 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace DatabaseSQLMusicApp
 {
     internal class AlbumsDAO
     {
-        // v1 contains fake data
-        List<Album> albums = new List<Album>
+        string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=music";
+        
+        public List<Album> getAllAlbums()
         {
+            List<Album> returnThese = new List<Album>();
+            MySqlConnection connection = new MySqlConnection(connectionString);
 
-        };
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT ID, ALBUM_TITLE, ARTIST, YEAR, IMAGE_NAME, DESCRIPTION FROM ALBUMS", connection);
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album a = new Album()
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        ImageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+                    returnThese.Add(a);
+                }
+            }
+
+            connection.Close();
+
+            return returnThese;
+        }
+
+        public List<Album> searchTitles(String searchTerm)
+        {
+            List<Album> returnThese = new List<Album>();
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            String searchWildPhrase = "%"+searchTerm+"%";
+            String query = "SELECT ID, ALBUM_TITLE, ARTIST, YEAR, IMAGE_NAME, DESCRIPTION " +
+                "FROM ALBUMS WHERE ALBUM_TITLE LIKE @search";
+
+            MySqlCommand command = new MySqlCommand();
+            command.CommandText = query;
+            command.Parameters.AddWithValue("@search", searchWildPhrase);
+            command.Connection = connection;
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album a = new Album()
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        ImageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+
+                    returnThese.Add(a);
+                }
+            }
+            connection.Close();
+
+            return returnThese;
+        }
     }
 }
