4f797b00fe62477d6cb9b9132767798342001592
jorgearmas <jorgearmasc0496@gmail.com>
2025-02-28 01:01:13 -0600

    Added getTracksForAlbums method

--- DatabaseSQLMusicApp/AlbumsDAO.cs
+++ DatabaseSQLMusicApp/AlbumsDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace DatabaseSQLMusicApp
 {
@@ -11,6 +12,7 @@ namespace DatabaseSQLMusicApp
     {
         string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=music";
         
+        /* --- GET DATA --- */
         public List<Album> getAllAlbums()
         {
             List<Album> returnThese = new List<Album>();
@@ -74,6 +76,60 @@ namespace DatabaseSQLMusicApp
                     returnThese.Add(a);
                 }
             }
+            connection.Close();
+
+            return returnThese;
+        }
+        
+        /* --- INSERT DATA --- */
+        public int addOneAlbum(Album album)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            String query = "INSERT INTO albums (ALBUM_TITLE, ARTIST, YEAR, IMAGE_NAME, DESCRIPTION) " +
+                "VALUES (@album_title, @artist, @year, @image_name, @description)";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@album_title", album.AlbumName);
+            command.Parameters.AddWithValue("@artist", album.ArtistName);
+            command.Parameters.AddWithValue("@year", album.Year);
+            command.Parameters.AddWithValue("@image_name", album.ImageURL);
+            command.Parameters.AddWithValue("@description", album.Description);
+            int new_rows = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            return new_rows;
+        }
+        
+        /* --- GET DATA / Track --- */
+        public List<Track> getTracksForAlbums(int albumID)
+        {
+            List<Track> returnThese = new List<Track>();
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            string query = "SELECT * FROM TRACKS WHERE albums_ID = @albumid";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@albumid", albumID);
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Track t = new Track()
+                    {
+                        ID = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Number = reader.GetInt32(2),
+                        VideoURL = reader.GetString(3),
+                        Lyrics = reader.GetString(4),
+                    };
+                    returnThese.Add(t);
+                }
+            }
+
             connection.Close();
 
             return returnThese;
